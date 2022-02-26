f069b147e1487098d4f940ca3bd1d9a0fa1f0842
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-02-26 02:04:01 -0600

    added flask messages letting the user know if the email was wrong, the password incorrect and when registering, if the email was already used to create an account

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def home():
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    error = None
     if request.method == 'POST':
         new_user = User(
             email=request.form.get('email'),
@@ -51,28 +52,38 @@ def register():
                 method='pbkdf2:sha256',
                 salt_length=8)
         )
-        db.session.add(new_user)
-        db.session.commit()
+        email_present = User.query.filter_by(email=new_user.email).first()
+        if email_present:
+            error = "You've already signed up with that email, please login instead."
+            return render_template("login.html", error=error)
+        else:
+            db.session.add(new_user)
+            db.session.commit()
 
-        login_user(new_user)
+            login_user(new_user)
 
-        return redirect(url_for("secrets"))
+            return redirect(url_for("secrets"))
 
     return render_template("register.html")
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    error = None
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
+        if not user:
+            error = 'That email does not exist, please try again.'
+        else:
+            if not check_password_hash(user.password, password):
+                error = 'Password incorrect, please try again.'
+            else:
+                login_user(user)
+                return redirect(url_for('secrets'))
 
-        if check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for('secrets'))
-
-    return render_template("login.html")
+    return render_template("login.html", error=error)
 
 
 @app.route('/secrets')
